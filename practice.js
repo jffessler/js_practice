1d3a5c9fc2bc8d5cf6166b0fd1d9f1e0cdf8d417
John Fessler <jffessler@gmail.com>
2023-11-12 13:36:27 -0800

    just practicing

---
+++ practice.js
@@ -0,0 +1,10 @@
+// console.log("Hello, World!")
+// console.log("Test")
+let variableName = "Welcome to Variables!!";
+const variableName1 = "you are not welcome!";
+variableName = "Hello!";
+var name = "John";
+
+console.log(name);
+console.log(variableName);
+console.log(variableName1);
